a44c2f2a26368463648bf7bdeb1b2337245b25df
Andrey <shuzlik@mail.ru>
2023-04-26 19:48:07 +0300

    fourth homework

---
+++ HW_3.sql
@@ -0,0 +1,64 @@
+USE hw_3;
+
+CREATE TABLE  AUTO 
+(       
+	REGNUM VARCHAR(10) PRIMARY KEY, 
+	MARK VARCHAR(10), 
+	COLOR VARCHAR(15),
+	RELEASEDT DATE, 
+	PHONENUM VARCHAR(15)
+);
+
+ -- AUTO
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111114,'LADA', 'КРАСНЫЙ', date'2008-01-01', '9152222221');
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111115,'VOLVO', 'КРАСНЫЙ', date'2013-01-01', '9173333334');
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111116,'BMW', 'СИНИЙ', date'2015-01-01', '9173333334');
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111121,'AUDI', 'СИНИЙ', date'2009-01-01', '9173333332');
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111122,'AUDI', 'СИНИЙ', date'2011-01-01', '9213333336');
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111113,'BMW', 'ЗЕЛЕНЫЙ', date'2007-01-01', '9214444444');
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111126,'LADA', 'ЗЕЛЕНЫЙ', date'2005-01-01', null);
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111117,'BMW', 'СИНИЙ', date'2005-01-01', null);
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111119,'LADA', 'СИНИЙ', date'2017-01-01', 9213333331);
+
+-- 1. Вывести количество машины каждого цвета для машин марок БМВ И ЛАДА
+SELECT MARK, COLOR, COUNT(COLOR) AS count
+FROM auto 
+WHERE MARK = 'BMW' OR MARK = 'LADA'
+GROUP BY MARK, COLOR;
+
+-- 2. Вывести марку авто и количество ато не этой марки
+-- 100 машин. 20 бмв, 80 другой. ауди 30 и 70 другой, лада 15 и 85 другой
+SELECT MARK, COUNT(*) AS count_mark,
+(SELECT COUNT(*) FROM auto) - COUNT(MARK) AS other
+FROM auto 
+GROUP BY MARK;
+
+-- 3 
+CREATE TABLE test_a (id INT, test VARCHAR(10));
+CREATE TABLE test_b (id INT);
+
+INSERT INTO test_a (id, test)
+VALUES
+(10, 'A'),
+(20, 'A'),
+(30, 'F'),
+(40, 'D'),
+(50, 'C');
+
+INSERT INTO test_b (id)
+VALUES
+(10),
+(30),
+(50);
+
+-- Вывести запрос, который вернет строки из таблицы test_a, id которых нет в таблице test_b
+SELECT * FROM test_a LEFT JOIN test_b USING(id) WHERE test_b.id IS NULL;
